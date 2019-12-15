ee441c738fb431235f361447229b10f05ad21d71
jheitmann <julien.heitmann@epfl.ch>
2019-12-15 10:16:59 +0100

    Added multithreading to NTA script

--- serialize_NTAs.py
+++ serialize_NTAs.py
@@ -1,12 +1,13 @@
-
+import json
 import numpy as np
 import pandas as pd
-import json
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
 from shapely.geometry import shape, Point
 
 
 path_nta = "data/Neighborhood Tabulation Areas.geojson"
-path_311 = "data/extract_corrupted.csv"
+path_311 = "data/311_Service_Requests_from_2010_to_Present_small.pkl"
 path_ctc_legend = "complaint_type_cls_legend.json"
 path_ctc = "complaint_type_cls.json"
 path_output = "NTA_and_CTC_output.pickle"
@@ -25,7 +26,15 @@ def get_nta(location, js, *, ct=None, print_step=None):
             return nta_code        
     return np.nan
 
-def convert(selected_ctcs, *, print_step=None):
+
+# Apply to dataframe
+def apply_to_df(df_chunks, js, print_params):
+    df_chunks["NTA"] = df_chunks["Location"].apply(lambda x: get_nta(x, js, **print_params))
+    return df_chunks
+    print("finished chunk")
+
+
+def convert(print_step=None):
     with open(path_ctc_legend) as fp_legend:
         ct_legend = json.load(fp_legend)
     with open(path_ctc) as fp_data:
@@ -33,7 +42,7 @@ def convert(selected_ctcs, *, print_step=None):
     with open(path_nta) as fp:
         js = json.load(fp)
 
-    df_source = pd.read_csv(path_311)
+    df_source = pd.read_pickle(path_311)
     print(df_source.shape[0], "total NTAs")
 
     # Keep entries with Latitude and Longitude
@@ -43,20 +52,30 @@ def convert(selected_ctcs, *, print_step=None):
     df["Complaint Type Class Index"] = df["Complaint Type"].apply(lambda k: ct_data[k] if k in ct_data else -1)
     ct_legend_inv = {v:k for k, v in ct_legend.items()}
     df["Complaint Type Class"] = df["Complaint Type Class Index"].apply(lambda k: ct_legend_inv[k])
-    df = df[df["Complaint Type Class"].isin(selected_ctcs)]
 
     print(df.shape[0], "filtered NTAs")
     
     # Create counter
     if print_step: ct = []
     print_params = dict(ct=[] if print_step else None, print_step=print_step)
-    df["NTA"] = df["Location"].apply(lambda x: get_nta(x, js, **print_params))
 
-    print(df.shape)
-    print(df["Complaint Type Class"].value_counts())
+    # Divide dataframe to chunks
+    prs = 100 # define the number of processes
+    chunk_size = int(df.shape[0]/prs)
+    chunks = [df.iloc[df.index[i:i + chunk_size]] for i in range(0, df.shape[0], chunk_size)]
+    
+    # Process dataframes
+    with ThreadPool(prs) as p:
+        result = p.map(apply_to_df, chunks, js, print_params)
 
-    df.to_pickle(path_output)
+    # Concat all chunks
+    df_reconstructed = pd.concat(result)
+
+    print(df_reconstructed.shape)
+    print(df_reconstructed["Complaint Type Class"].value_counts())
+
+    df_reconstructed.to_pickle(path_output)
 
 if __name__=="__main__":
-    convert(["noise annoyance", "pest"], print_step=1000)
+    convert(print_step=1000)
     print(pd.read_pickle(path_output).head())
